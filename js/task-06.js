7155dd2878cca7c14604f0102bd619f180572868
BohdanDehodiuk <86683356+BohdanDehodiuk@users.noreply.github.com>
2021-11-09 15:46:41 +0200

    finish HW 6 5.0

--- js/task-06.js
+++ js/task-06.js
@@ -4,14 +4,16 @@ input.addEventListener("blur", changeInputColor);
 
 function changeInputColor(event) {
   console.log(event.currentTarget.value.length);
-  if (
-    Number.parseInt(input.dataset.length) === event.currentTarget.value.length
-  ) {
-    return input.classList.add("valid");
-  }
+
   if (
     Number.parseInt(input.dataset.length) !== event.currentTarget.value.length
   ) {
     return input.classList.add("invalid");
+  }
+
+  if (
+    Number.parseInt(input.dataset.length) === event.currentTarget.value.length
+  ) {
+    return input.classList.replace("invalid", "valid");
   }
 }
--- js/task-09.js
+++ js/task-09.js
@@ -2,22 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// const refs = {
-//   colorName: document.querySelector(".color"),
-//   btn: document.querySelector(".change-color"),
-//   body: document.querySelector("body"),
-// };
+const refs = {
+  colorName: document.querySelector(".color"),
+  btn: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+};
 
-// refs.btn.addEventListener("click", () => {
-//   refs.body.style.backgroundColor = getRandomHexColor();
-//   refs.colorName.textContent = getRandomHexColor();
-// });
-
-const button = document.querySelector(".change-color");
-const span = document.querySelector("span");
-
-button.addEventListener("click", () => {
-  document.body.style.backgroundColor = getRandomHexColor();
-  span.textContent = getRandomHexColor();
-  console.log("Button was clicked and changed a background-color");
+refs.btn.addEventListener("click", () => {
+  const hexColorName = getRandomHexColor();
+  refs.body.style.backgroundColor = `${hexColorName}`;
+  refs.colorName.textContent = `${hexColorName}`;
 });
